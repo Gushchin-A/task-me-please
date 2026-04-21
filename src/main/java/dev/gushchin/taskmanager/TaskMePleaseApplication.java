26a4fe5084c0a90ad21effb799214e063cd301ba
Andrei Gushchin <kpa6ep@gmail.com>
2026-04-21 22:07:19 +0200

    initial commit

---
+++ src/main/java/dev/gushchin/taskmanager/TaskMePleaseApplication.java
@@ -0,0 +1,13 @@
+package dev.gushchin.taskmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMePleaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMePleaseApplication.class, args);
+	}
+
+}
