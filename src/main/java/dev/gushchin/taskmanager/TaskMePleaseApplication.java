9b47182f0d7689c5cad43940b9febf445aefb98b
Andrei Gushchin <kpa6ep@gmail.com>
2026-04-22 22:58:06 +0200

    checkstyle, spotless, pmd

--- src/main/java/dev/gushchin/taskmanager/TaskMePleaseApplication.java
+++ src/main/java/dev/gushchin/taskmanager/TaskMePleaseApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskMePleaseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(TaskMePleaseApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(TaskMePleaseApplication.class, args);
+    }
 }
